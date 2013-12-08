821e1602b01a4f159d76711a90a24cf668acd014
Daryl Lozupone <daryl@clubduece.com>
2013-12-08 14:37:31 -0500

    Updated documentation to version 0.2

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Base_Controller"],["c","Base_Controller_CPT"],["c","Base_Controller_Plugin"],["c","Base_Controller_Settings"],["c","Base_Helpers_Render_Fields"],["c","Base_Model"],["c","Base_Model_CPT"],["c","Base_Model_Help_Tab"],["c","Base_Model_JS_Object"],["c","Base_Model_Menu_Page"],["c","Base_Model_Metabox"],["c","Base_Model_Plugin"],["c","Base_Model_Settings"],["c","Helper_Functions"]];
+ApiGen.elements = [["c","Base_Controller"],["c","Base_Controller_CPT"],["c","Base_Controller_Plugin"],["c","Base_Controller_Settings"],["c","Base_Helpers_Render_Fields"],["c","Base_Model"],["c","Base_Model_Admin_Notice"],["c","Base_Model_CPT"],["c","Base_Model_Help_Tab"],["c","Base_Model_JS_Object"],["c","Base_Model_Menu_Page"],["c","Base_Model_Metabox"],["c","Base_Model_Plugin"],["c","Base_Model_Settings"],["c","Helper_Functions"]];
